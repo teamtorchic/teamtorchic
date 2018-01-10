36d12b90d376a419d29cb3f4315ad4034e7be456
David Bruhnn <daviddbruhn@gmail.com>
2018-01-09 19:42:19 -0800

    index.jsx

---
+++ react-client/src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <div></div>
+    )
+  }
+
+}
