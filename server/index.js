00c9d89c3b7fc7550074678075aa3a9fcac0e3db
Lory Zhang <zhang.rouyun@gmail.com>
2018-01-12 15:39:13 -0800

    trying to console.log some params from oAuth callbacks

--- server/index.js
+++ server/index.js
@@ -11,22 +11,21 @@ const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 //   credentials (in this case, an accessToken, refreshToken, and Google
 //   profile), and invoke a callback with a user object.
 passport.use(new GoogleStrategy({
-    clientID: '862249102823-aalhroidkbb9r5ic3dfnfuinqb5nkd6b.apps.googleusercontent.com',
-    clientSecret: 'Bh8gjZLqLzMPb1F3dh2UgxDk',
-    callbackURL: "https://eatchictorchic.herokuapp.com/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-      //  User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      //    return done(err, user);
-      //  });
-      const fake = {
-        username: 'Lory',
-        id: 3,
-        password: '123',
-      }
-      return done(err, fake);
-  }
-));
+  clientID: '862249102823-aalhroidkbb9r5ic3dfnfuinqb5nkd6b.apps.googleusercontent.com',
+  clientSecret: 'Bh8gjZLqLzMPb1F3dh2UgxDk',
+  callbackURL: 'https://eatchictorchic.herokuapp.com/auth/google/callback',
+}, (accessToken, refreshToken, profile, done) => {
+  console.log (accessToken, refreshToken, profile);
+  //  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+  //    return done(err, user);
+  //  });
+  const fake = {
+    username: 'Lory',
+    id: 3,
+    password: '123',
+  };
+  return done(fake);
+}));
 
 // Router
 const router = require('./routes.js');
@@ -47,11 +46,9 @@ app.get('/auth/google',
 //   request.  If authentication fails, the user will be redirected back to the
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
-app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
+  res.redirect('/');
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
