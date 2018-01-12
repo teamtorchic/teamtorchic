348ee2a337774e57825fc28823329c1a031acbbc
Lory Zhang <zhang.rouyun@gmail.com>
2018-01-11 19:46:00 -0800

    downvote post request backend working

--- server/models/index.js
+++ server/models/index.js
@@ -44,11 +44,33 @@ module.exports = {
         });
     },
     downVote: (dishId, likesDish, userId, restaurantId) => {
-      const updateDownVote = {
+      const checkVote = {
+        text: 'select likesDish, id from posts where userId = $1 and dishId = $2',
+        values: [userId, dishId],
+        rowMode: 'array',
+      };
+      const insertVote = {
         text: 'insert into posts (likesDish, userId, dishId, restaurantId) values ($1, $2, $3, $4)',
         values: [dishId, likesDish, userId, restaurantId],
       };
-      return db.client.query(updateDownVote);
+      return db.client.query(checkVote)
+        .then((data) => {
+          if (data.rows.length) {
+            let notLike = data.rows[0][0];
+            const updateId = data.rows[0][1];
+            if (notLike === 0) {
+              notLike = null;
+            } else {
+              notLike = 0;
+            }
+            const updateDownVote = {
+              text: 'update posts set likesDish = $1 where id = $2',
+              values: [notLike, updateId],
+            };
+            return db.client.query(updateDownVote);
+          }
+          return db.client.query(insertVote);
+        });
     },
   },
 };
