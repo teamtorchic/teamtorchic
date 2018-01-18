b6d4150f99196e7c242d43ea45d8e94b54ebd4f9
David Bruhnn <daviddbruhn@gmail.com>
2018-01-17 20:43:11 -0800

    comments.js

--- client/src/components/comment.jsx
+++ client/src/components/comment.jsx
@@ -9,9 +9,13 @@ class Comment extends React.Component {
     this.state = {
       comment: '',
       comments: [],
+      reviews: [],
+      likes: 0,
+      userLikes: false,
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleEnter = this.handleEnter.bind(this);
+    this.handleLike = this.handleLike.bind(this);
 
     console.log('posts ', this.props.post);
 
@@ -22,19 +26,45 @@ class Comment extends React.Component {
     }).done((data) => {
       this.setState({ comments: data.rows });
     });
+    $.get({
+      url: '/likes',
+      data: { post: this.props.post, user: this.props.currentUser },
+      contentType: 'application/json',
+    }).done((data) => {
+      console.log(data);
+      this.setState({ likes: data.count, userLikes: data.usersLike });
+    });
   }
 
   handleChange(event) {
     this.setState({ comment: event.target.value });
   }
 
+  handleLike() {
+    const newOpinion = !this.state.userLikes;
+    $.post({
+      url: '/likes',
+      data: JSON.stringify({ likes: newOpinion, post: this.props.post, user: this.props.currentUser }),
+      contentType: 'application/json',
+    });
+
+    if (this.state.userLikes) {
+      this.setState({ likes: this.state.likes - 1 });
+    } else {
+      this.setState({ likes: this.state.likes + 1 });
+    }
+
+    this.setState({ userLikes: !this.state.userLikes });
+  }
+
   handleEnter(event) {
     if (event.key === 'Enter') {
+      console.log('this.state.currentUser = ', this.state.currentUser);
       const content = event.target.value;
       $.post({
         url: '/comments',
         contentType: 'application/json',
-        data: JSON.stringify({ comment: content, userId: 2, postId: this.props.post }),
+        data: JSON.stringify({ comment: content, userId: this.props.currentUser, postId: this.props.post }),
       }).done(() => {
         this.setState({ comment: '' });
         $.get({
@@ -54,6 +84,48 @@ class Comment extends React.Component {
 
     return (
       <div className="comments">
+        <div className="row">
+          <div className="col-10">
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
+                <button
+                  onClick={this.handleLike}
+                  className="btn btn-outline-secondary likes-button"
+                  type="button"
+                >
+                  <i className={`material-icons ${this.state.userLikes}`}>favorite_border</i>&nbsp;{this.state.likes}
+                </button>
+              </div>
+              <button className="btn btn-outline-secondary" type="button">
+                <i className="material-icons">mood</i> 7
+              </button>
+              <button className="btn btn-outline-secondary" type="button">
+                <i className="material-icons">mood_bad</i> 2
+              </button>
+            </div>
+          </div>
+          <div className="col">
+            <div className="input-group mb-3">
+              {(this.state.comments.length > 0) &&
+                <button
+                  type="button"
+                  className="form-control"
+                  aria-label=""
+                  aria-describedby="basic-addon1"
+                ><i className="material-icons">message</i> {this.state.comments.length}
+                </button>}
+              {(this.state.reviews.length > 0) &&
+              <button
+                type="button"
+                className="form-control"
+                aria-label=""
+                aria-describedby="basic-addon1"
+              ><i className="material-icons">star_rate</i> {this.state.reviews.length}
+              </button>}
+            </div>
+          </div>
+        </div>
+
         <textarea
           placeholder="Leave a comment..."
           className="leaveComment form-control"
@@ -61,7 +133,6 @@ class Comment extends React.Component {
           onChange={this.handleChange}
           onKeyPress={this.handleEnter}
         />
-        {this.state.comments.length > 0 && <ul>{comments}</ul> }
       </div>);
   }
 }
