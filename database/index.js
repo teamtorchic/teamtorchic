1bd60385ab53ecd416a9b0e307d5bbd949acd81d
David Bruhnn <daviddbruhn@gmail.com>
2018-01-20 12:44:40 -0800

    update database file to  Lory's edits

--- database/index.js
+++ database/index.js
@@ -1,19 +1,15 @@
-const { Pool, Client } = require('pg');
+const { Client } = require('pg');
 
-const pool = new Pool({
-  user: 'eatchic',
-  password: 'eatchic',
-  database: process.env.database,
-});
-
-pool.query('select NOW()', (err, res) => {
+// const pool = new Pool({
+//   user: 'eatchic',
+//   password: 'eatchic',
+//   database: process.env.database,
+// });
+//
+// pool.query('select NOW()', (err, res) => {
   // console.log(err, res);
-});
+// });
 
-const client = new Client({
-  user: 'eatchic',
-  password: 'eatchic',
-  database: process.env.database,
-});
+const client = new Client(process.env.database);
 
-module.exports = { pool, client };
+module.exports = { client };
