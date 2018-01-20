9977fa5bc8c2281f20929613764258cbcfc73299
Lory Zhang <zhang.rouyun@gmail.com>
2018-01-20 11:50:44 -0800

    updated database connnection uri

--- database/index.js
+++ database/index.js
@@ -1,19 +1,15 @@
 const { Pool, Client } = require('pg');
 
-const pool = new Pool({
-  user: 'eatchic',
-  password: 'eatchic',
-  database: 'eat_chic',
-});
+// const pool = new Pool({
+//   user: 'eatchic',
+//   password: 'eatchic',
+//   database: 'eat_chic',
+// });
 
-pool.query('select NOW()', (err, res) => {
-  // console.log(err, res);
-});
+// pool.query('select NOW()', (err, res) => {
+//   // console.log(err, res);
+// });
 
-const client = new Client({
-  user: 'eatchic',
-  password: 'eatchic',
-  database: 'eat_chic',
-});
+const client = new Client(process.env.DATABASE_URL);
 
 module.exports = { pool, client };
