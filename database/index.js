96df5029ebe146d4013bf3220dddff073cc30b02
David Bruhnn <daviddbruhn@gmail.com>
2018-01-20 12:33:38 -0800

    update database to use system variable

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ const { Pool, Client } = require('pg');
 const pool = new Pool({
   user: 'eatchic',
   password: 'eatchic',
-  database: 'eat_chic',
+  database: process.env.database,
 });
 
 pool.query('select NOW()', (err, res) => {
@@ -13,7 +13,7 @@ pool.query('select NOW()', (err, res) => {
 const client = new Client({
   user: 'eatchic',
   password: 'eatchic',
-  database: 'eat_chic',
+  database: process.env.database,
 });
 
 module.exports = { pool, client };
